cc1fe3dcb4c213de6be80a1d9e52e18771f99f76
MartinKayz <kubonamartinyafesi132@gmail.com>
2021-08-24 23:50:51 +0300

    empty file, later use

--- CapStone-Web-App-Flask/models.py
+++ CapStone-Web-App-Flask/models.py
@@ -1,18 +1,2 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class TrialData(db.Model):
-    __tablename__ = 'trial_data'
-
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String())
-    age = db.Column(db.Integer())
- 
-    def __init__(self, name,age):
-        self.name = name
-        self.age = age
- 
-    def __repr__(self):
-        return f"{self.name}:{self.age}"
+# to be used later on
 
